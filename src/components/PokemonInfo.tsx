8efcd6a941f13fe0a85fe6e1c8b46e3aa05be67c
jmanzanares12 <jimgken2004@gmail.com>
2025-04-25 23:35:32 -0600

    try: use API for make PokemonInfo component

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useGetPokemon } from '../hooks/useGetPokemon';
 import { useParams } from 'react-router-dom';
+import { getMainPokemonType } from '../utils/getMainPokemonType';
+
 
 const PokemonInfo = () => {
-    const { pokemonId } = useParams();
-    const { data: pokemonData } = useGetPokemon(pokemonId);
+    const { pokemonId } = useParams<{pokemonId: string}>();
+    const { data: pokemonData } = useGetPokemon(pokemonId ?? undefined);
+    const mainType = useMemo(() => {
+        if (!pokemonData) return null;
+        return getMainPokemonType(pokemonData);
+    }, [pokemonData]);
+    
 
     return (
-        <div>
-            <h1>{pokemonData?.name}</h1>
+        <div className='flex flex-row justify-center shadow-lg bg-gray-100 rounded-md p-4 m-4'>
+            <div className={`${mainType ?? 'default'}-background w-72 h-72 rounded-l-lg items-center`}>
+                <img src={pokemonData?.sprites?.front_default} alt={pokemonData?.name ?? ''} className='w-full h-full' />
+            </div>
         </div>
     )
 }
