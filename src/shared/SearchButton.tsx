6fda513e4a95ed66c7fb6adcec23dd3309109bd3
jmanzanares12 <jimgken2004@gmail.com>
2025-05-01 23:41:07 -0600

    Improve: SearchButton

--- src/shared/SearchButton.tsx
+++ src/shared/SearchButton.tsx
@@ -2,7 +2,7 @@ import { useSearchStore } from '../store/useSearchStore';
 
 const SearchButton = () => {
     const openModal = useSearchStore((state) => state.openModal);
-    return <button onClick={openModal}>Search</button>
+    return <button onClick={openModal} className='text-black hover:text-white'>Search</button>
 }
 
 export default SearchButton;
